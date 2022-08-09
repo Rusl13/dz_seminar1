b1f9c8e13a294f104f4a2f0f31d7d838108b938a
Rusl <hovarus@bk.ru>
2022-08-09 15:50:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача 2: Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число большее, а
+//какое меньшее.
+
+/* Console.WriteLine("Ввведите число a");
+Console.WriteLine("Ввведите число b ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if ( a>b )
+{
+Console.WriteLine(" Большее число :" +a );
+Console.WriteLine("Меньшее число :" +b );
+}
+else 
+Console.WriteLine(" Большее число :" +b );
+Console.WriteLine("Меньшее число :" +a ); */
+
+
+//Задача 4: Напишите программу, которая принимает на
+//вход три числа и выдаёт максимальное из этих чисел.
+
+/* Console.WriteLine("Ввведите число a");
+Console.WriteLine("Ввведите число b");
+Console.WriteLine("Ввведите число c");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.WriteLine(" Большее число :" +max ); */
+
+
+
+//Задача 6: Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+
+/* Console.WriteLine("Ввведите число a");
+int a = int.Parse(Console.ReadLine());
+
+if (a%2==0)
+{
+   Console.WriteLine("число четное :" +a ); 
+}
+else 
+Console.WriteLine("число нечетное :" +a ); 
+ */
+
+ //Задача 8: Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+
+/* Console.Write("Ввведите число N");
+int N = int.Parse(Console.ReadLine());
+
+int i=1;
+
+while (i<=N)
+{
+    if(i%2==0)
+    {
+        Console.WriteLine(i);
+        
+    }
+    i++;
+}
+ */
+
